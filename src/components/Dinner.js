7ffb7e1b8415b1c98ea652f64e1d7dbfc60643b2
Farnoosh <91014204+Farnooshmo@users.noreply.github.com>
2024-02-13 00:16:05 +0200

    Create Dinner.js

---
+++ src/components/Dinner.js
@@ -0,0 +1,18 @@
+function Dinner() {
+  return (
+    <div>
+      <label for="dinner">Dinner</label>
+      <select id="dinner" name="dinner">
+        <option value="dinner" selected>
+          dinner
+        </option>
+        <option value="cheese">Cheese</option>
+        <option value="egg">Egg</option>
+        <option value="yoghurt">Yoghurt</option>
+        <option value="salade">Salade</option>
+      </select>
+    </div>
+  );
+}
+
+export default Dinner;
