37814f418147b139ed51f3a31986fccb7e8acddb
Farnoosh <91014204+Farnooshmo@users.noreply.github.com>
2024-02-13 00:01:00 +0200

    Create DietForm.js

---
+++ src/components/DietForm.js
@@ -0,0 +1,18 @@
+function DietForm() {
+  return (
+    <div>
+      <label for="breakfast">Breakfast</label>
+      <select id="breakfast" name="breakfast">
+        <option value="breakfast" selected>
+          Breakfast
+        </option>
+        <option value="cheese">Cheese</option>
+        <option value="egg">Egg</option>
+        <option value="Yoghurt">Yoghurt</option>
+      </select>
+      
+    </div>
+  );
+}
+
+export default DietForm;
