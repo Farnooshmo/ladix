0b8ef0c30fe760491d820f904204a27f657f9d81
Farnoosh <91014204+Farnooshmo@users.noreply.github.com>
2024-02-13 00:22:54 +0200

    Update DietForm.js

--- src/components/DietForm.js
+++ src/components/DietForm.js
@@ -1,11 +1,15 @@
 import Breakfast from "./Breakfast";
 import Lunch from "./Lunch";
+import Dinner from "./Dinner";
+import Snack from "./Snack";
 
 function DietForm() {
   return (
     <div>
       <Breakfast />
       <Lunch />
+      <Dinner />
+      <Snack />
     </div>
   );
 }
